b5e5fecf11b48ad89680ad6ca3065c3bb5b0c4e8
Adib Rastegarnia <adib.rastegarnia@intel.com>
2023-02-13 12:42:54 -0800

    Add required flags to main (#2)
    * Add required flags to main
    
    * bump version

--- cmd/fabric-underlay/fabric-underlay.go
+++ cmd/fabric-underlay/fabric-underlay.go
@@ -14,12 +14,18 @@ import (
 
 var log = logging.GetLogger()
 
+const (
+	topoAddressFlag    = "topo-address"
+	defaultTopoAddress = "onos-topo:5150"
+)
+
 // The main entry point
 func main() {
 	cmd := &cobra.Command{
 		Use:  "fabric-underlay",
 		RunE: runRootCommand,
 	}
+	cmd.Flags().String(topoAddressFlag, defaultTopoAddress, "address:port or just :port of the onos-topo service")
 	cli.AddServiceEndpointFlags(cmd, "fabric underlay gRPC")
 	cli.Run(cmd)
 }
@@ -29,10 +35,12 @@ func runRootCommand(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
+	topoAddress, _ := cmd.Flags().GetString(topoAddressFlag)
 
 	log.Infof("Starting fabric-underlay")
 	cfg := manager.Config{
 		ServiceFlags: flags,
+		TopoAddress:  topoAddress,
 	}
 	return cli.RunDaemon(manager.NewManager(cfg))
 }
